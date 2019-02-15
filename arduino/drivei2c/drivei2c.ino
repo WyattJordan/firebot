69f9b7cbf44ab4ba19e35b2e576815b2ad2f9b0d
Wyatt Jordan <wyattsjordan@gmail.com>
2019-02-15 17:21:26 -0500

    arduino new motor protocol

--- arduino/drivei2c/drivei2c.ino
+++ arduino/drivei2c/drivei2c.ino
@@ -21,13 +21,15 @@ byte rightPinALast;
 int rightDuration;//the number of the pulses
 boolean rightDirection;//the rotation direction
 
-unsigned char lPWM, rPWM;
-boolean lforward, rforward;
+unsigned char lPWM, rPWM, lPWM2, rPWM2;
+boolean lforward, rforward, lforward2, rforward2, zeroFlag;
+long zeroStamp;
+boolean sDebug;
 
 char sendbuff [100];
 char  getBuff [100];
 float time2;
-int encCount, motCount;
+int encCount, motCount, motCount2;
 void setup() {
   // I2C
   Wire.begin(17); 
@@ -43,9 +45,11 @@ void setup() {
   attachInterrupt(0, leftwheelSpeed, CHANGE);//int.0 (pin2)
   attachInterrupt(1, rightwheelSpeed,CHANGE);//int.1 (pin3)
   encCount = motCount = -1; // counts down to 0
+  zeroFlag = false;
+  zeroStamp = millis();
   
   // drive
-  lPWM = rPWM = 0; // crawling forward
+  lPWM = rPWM = 0; // stopped
   lforward = rforward = true;
   pinMode(leftDrivePin, OUTPUT);
   pinMode(rightDrivePin, OUTPUT);
@@ -55,6 +59,9 @@ void setup() {
   analogWrite(rightDrivePin, rPWM);
   digitalWrite(leftDirPin, HIGH);
   digitalWrite(rightDirPin, HIGH);  
+
+  sDebug = false;
+  if(sDebug) Serial.begin(9600);
   
   for(int i=0; i<3; i++){
     digitalWrite(LED_BUILTIN, LOW);
@@ -66,29 +73,52 @@ void setup() {
 }
 
 void loop() {
-  /*if(millis() - time2 > 1000){
-    digitalWrite(LED_BUILTIN, LOW);
-  }*/
+  if(sDebug){
+        Serial.print("lPWM = ");
+        Serial.print(lPWM);
+        Serial.print(" rPWM = ");
+        Serial.print(rPWM);
+         Serial.print(" lPWM2 = ");
+        Serial.print(lPWM2);
+        Serial.print(" rPWM2 = ");
+        Serial.println(rPWM2);
+      }
+  if(zeroFlag){
+      digitalWrite(LED_BUILTIN, HIGH);
+      zeroFlag = false;
+  }
+  if(millis()-zeroStamp > 500){      
+      digitalWrite(LED_BUILTIN, LOW);
+  }
 }
 
 void getData(int num){
   getBuff[0] = Wire.read();
   if(getBuff[0] == '1'){ encCount = 4;} // starts 4 byte enc interaction
-  if(getBuff[0] == 'a' || getBuff[0] == 'b'){ motCount = 4;} // starts 3 byte motor interaction
+  else if(getBuff[0] == 'a' || getBuff[0] == 'b'){ motCount = 4;} // starts 3 byte motor interaction
+  else if(getBuff[0] == 'w' || getBuff[0] == 'x'){ motCount2 = 4;}
   
   // leftmotor is a/b and right is c/d (first char is forrward)
   if(motCount == 4) lforward = getBuff[0] == 'a' ? true : false;
   if(motCount == 3) lPWM = getBuff[0];
   if(motCount == 2) rforward = getBuff[0] == 'c' ? true : false;
   if(motCount == 1) rPWM = getBuff[0];
+
+  // leftmotor is w/x and right is y/z (first char is forrward)
+  if(motCount2 == 4) lforward2 = getBuff[0] == 'w' ? true : false;
+  if(motCount2 == 3) lPWM2 = getBuff[0];
+  if(motCount2 == 2) rforward2 = getBuff[0] == 'y' ? true : false;
+  if(motCount2 == 1) rPWM2 = getBuff[0];
+
   
-  
-}
+  if(rPWM == 0 || lPWM == 0 || lPWM==50 || rPWM==52) {
+    zeroFlag = true; zeroStamp = millis();}
+} // end of getData
 
 void sendData(){
   
   if(encCount > 0){
-    // incoming bytes should be ['l',lPWM,'r',rPWM], send back enc vals
+    // incoming bytes should be ['1','2','3','4'], send back enc vals
     if(encCount == 4){
       Wire.write(highByte(leftDuration));
     }
@@ -105,31 +135,61 @@ void sendData(){
     }
     encCount--;
     time2 = millis();
-    digitalWrite(LED_BUILTIN, HIGH);
   }
+  // first motor interaction incoming bytes should be ['a/b',lPWM,'c/d',rPWM]
   else if(motCount>0){
-    // incoming bytes should be ['m',lPWM,rPWM], send back nothing
     if(motCount == 4){
-        
+        Wire.write(lforward ? 'a' : 'b');
     }
     if(motCount == 3){
       Wire.write(lPWM); // send back for verification
-      analogWrite(leftDrivePin, lPWM);
-      digitalWrite(leftDirPin, lforward ? HIGH : LOW);
     }
     if(motCount == 2){
-      
+      Wire.write(rforward ? 'c' : 'd');
     }
     if(motCount == 1){
       Wire.write(rPWM); // send back for verification
-      analogWrite(rightDrivePin, rPWM);
-      digitalWrite(rightDirPin, rforward ? HIGH : LOW);
     }
-    motCount--; 
-    
+    motCount--;    
+  }
+  // second motor interaction to verify, only set PWMs if both agree
+  // incoming bytes should be ['w/x',lPWM,'y/z',rPWM]
+  else if(motCount2>0){
+    if(motCount2 == 4){
+        Wire.write(lforward2 ? 'w' : 'x');
+    }
+    if(motCount2 == 3){
+      if(lPWM == lPWM2 && (lforward == lforward2)){
+        Wire.write(lPWM2); // send back for verification
+        analogWrite(leftDrivePin, lPWM);
+        digitalWrite(leftDirPin, lforward ? HIGH : LOW);
+      }
+      else{
+        Wire.write('q'); // the two messages don't match
+      }
+      
+    }
+    if(motCount2 == 2){
+      Wire.write(rforward2 ? 'y' : 'z');
+    }
+    if(motCount2 == 1){
+      if(rPWM == rPWM2 && (rforward == rforward2)){
+        Wire.write(rPWM2); // send back for verification
+        analogWrite(rightDrivePin, rPWM);
+        digitalWrite(rightDirPin, rforward ? HIGH : LOW);
+      }
+      else{
+        Wire.write('r');
+      }
+     
+    }
+    motCount2--;   
   }
 }
 
+////////////////////////////////////////////////////////////////
+///////////////////Encoder Interrupts///////////////////////////
+////////////////////////////////////////////////////////////////
 void leftwheelSpeed()
 {
   int Lstate = digitalRead(leftpinA);
